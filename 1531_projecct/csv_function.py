ac218e3f910f649a851020d3823585e2803a2339
ManyJohn <turlyjohn@foxmail.com>
2017-08-23 21:44:36 +1000

    Building up function for US-A4
    adding functions for :
    1. adding questions to question pool
    2.create new survey

---
+++ 1531_projecct/csv_function.py
@@ -0,0 +1,82 @@
+import csv
+import os
+import ast
+
+
+
+def take_in_qustion(question,option):
+            
+
+    with open('question_pool.csv','a') as csv_out:
+            with open('question_pool.csv','r') as csv_in:
+                reader = csv.reader(csv_in)
+                i=len(list(reader))
+                print(i)
+                content=[]
+                content.append(i)
+                content.append(question)
+                option=str.split(option,',')
+                for each_op in option:
+                    content.append(each_op)
+
+            writer = csv.writer(csv_out,quoting=csv.QUOTE_ALL)
+            writer.writerow(content) 
+            
+
+
+def output_qurstion():
+      question_list=[]
+      with open('question_pool.csv','r') as csv_in:
+        reader = csv.reader(csv_in)
+        for row in reader:
+           question_list.append(row)
+        return question_list
+
+
+def output_course():
+    course_list=[]
+    with open('courses.csv','r') as csv_in:
+        reader = csv.reader(csv_in)
+        i=0
+       
+        for row in reader:
+           
+            #name=row
+            if (i==0):
+                i=i+1
+                continue
+            if (len(row)==0):
+                continue
+
+            name=row[0]
+            #if str.isspace(name):
+            #    continue
+            course_list.append(name)
+
+    return course_list
+
+def make_survey(file_name,question_list):
+    survey_dir="./survey/"
+    if not os.path.exists(survey_dir):
+        os.makedirs(survey_dir)
+    #question_file=open("question_pool.csv","r")
+    with open('question_pool.csv','r') as csv_in:
+        question_reader = csv.reader(csv_in,quoting=csv.QUOTE_ALL)
+        question_reader =list(question_reader)
+    with open(survey_dir+file_name+".csv",'a') as csv_out:
+        writer = csv.writer(csv_out)
+        for question_id in question_list:
+            options_of_each_question=ast.literal_eval(question_reader[int(question_id)][2])
+            print(options_of_each_question[0])
+            num_of_options=len(options_of_each_question)
+            writer.writerow([question_id,[0]*num_of_options]) 
+            
+#question=input("what is the question?")
+#option=str.split(input("enter option, and sperate them in comma"),',')
+#print(option[0])
+#take_in_qustion(question,option)
+
+
+
+#make_survey(file_name="name",question_list=[0])
+
---
+++ 1531_projecct/routes.py
@@ -0,0 +1,56 @@
+# create a Flask application instance
+from flask import Flask, redirect, render_template, request, url_for
+from server import app
+from csv_function import *
+# define a route through the app.route decorator
+session={}
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        if request.form['bt']=="course":
+            return redirect(url_for("choose_course"))
+    return render_template("index.html",add_question_link=url_for("add_question"))
+
+
+@app.route("/add_question", methods=["GET", "POST"])
+def add_question(): 
+    if request.method == "POST":
+        if request.form['bt']=="submit":
+            question_get=request.form['question']
+            option_get=request.form['option']
+            take_in_qustion(question_get,option_get)
+    return render_template("add_question.html")
+
+
+@app.route("/error")
+def error():
+    print ("<h1>404!!!!!!!!!!!!!!!!!!!!!!</h1>")
+    return "<h1>403</h1>"
+
+
+@app.route("/courses", methods=["GET", "POST"])
+def choose_course():
+    course_list=output_course()
+    if request.method=="POST":
+        if request.form['bt']=="submit":
+            choice=request.form["choice"]
+            print(choice)
+            return  redirect(url_for("create_survey",course_name=choice))
+    return render_template("course_list.html",course=course_list)
+
+
+@app.route("/create_survey/<course_name>",methods=["GET", "POST"])
+def create_survey(course_name):
+    #print(course_name)
+    all_question=output_qurstion()
+    if request.method=="POST":
+        if request.form["bt"]=="submit":
+            question_selected=request.form.getlist("question")
+            print (question_selected)
+            make_survey(file_name=course_name,question_list=question_selected)
+    return  render_template("create_survey.html",course=course_name,question_list=all_question)
+
+# launch the integrated development web server
+# and run the app on http://localhost:8085
+#if __name__=="__main__":
+#   app.run(debug=True,port=8085)
